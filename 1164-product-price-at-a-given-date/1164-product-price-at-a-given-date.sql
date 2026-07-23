b4082472bf9d6820e58114d8cfed73110d8b8c3a
Mohammad Ifaz Alamgir <ifazalamgir7@gmail.com>
2026-07-23 13:42:50 +0600

    Time: 480 ms (66.77%), Space: 0B (100%) - LeetHub

--- 1164-product-price-at-a-given-date/1164-product-price-at-a-given-date.sql
+++ 1164-product-price-at-a-given-date/1164-product-price-at-a-given-date.sql
@@ -1,15 +1,19 @@
 SELECT
-    p.product_id,
-    COALESCE(
-        (
-            SELECT new_price
-            FROM Products p2
-            WHERE p2.product_id = p.product_id
-              AND p2.change_date <= '2019-08-16'
-            ORDER BY change_date DESC
-            LIMIT 1
-        ),
-        10
-    ) AS price
-FROM Products p
-GROUP BY p.product_id;
+    product_id,
+    10 AS price
+FROM Products
+GROUP BY product_id
+HAVING MIN(change_date) > '2019-08-16'
+
+UNION
+
+SELECT
+    product_id,
+    new_price AS price
+FROM Products
+WHERE (product_id, change_date) IN (
+    SELECT product_id, MAX(change_date)
+    FROM Products
+    WHERE change_date <= '2019-08-16'
+    GROUP BY product_id
+);
