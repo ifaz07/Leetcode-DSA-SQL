f4c8964b2636f4176b61ce837df6404f62917ac5
Mohammad Ifaz Alamgir <ifazalamgir7@gmail.com>
2026-08-25 12:15:55 +0600

    Time: 2 ms (33.33%), Space: 12.5 MB (19.54%) - LeetHub

---
+++ 3718-smallest-missing-multiple-of-k/3718-smallest-missing-multiple-of-k.py
@@ -0,0 +1,10 @@
+class Solution(object):
+    def missingMultiple(self, nums, k):
+        nums_set = set(nums)
+
+        multiple = k
+
+        while multiple in nums_set:
+            multiple += k
+
+        return multiple
